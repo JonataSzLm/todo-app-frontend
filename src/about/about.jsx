56a67ddcaf0f1dc6f39a4b76fadda106e3cd1b55
JonataSzLm <jonaatslima5@gmail.com>
2022-10-15 14:28:42 -0300

    Fim de modulo

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import PageHeader from "../template/pageHeader"
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sunt beatae perferendis, quis laborum amet dicta neque maxime eligendi quasi id ducimus ut odit quos unde quia consequuntur ea sequi.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sunt beatae perferendis, quis laborum amet dicta neque maxime eligendi quasi id ducimus ut odit quos unde quia consequuntur ea sequi.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sunt beatae perferendis, quis laborum amet dicta neque maxime eligendi quasi id ducimus ut odit quos unde quia consequuntur ea sequi.</p>
+    </div>
+)
